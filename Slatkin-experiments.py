1f40b36694e15e9cc3f38b72bbc80290517185f4
hattwick <philhatt@mac.com>
2015-03-18 16:21:13 -0400

    library import

--- Slatkin-experiments.py
+++ Slatkin-experiments.py
@@ -2,4 +2,9 @@
 # __author__ = 'phil'
 # Mostly collection of small style routines
 
+import urllib
+
+from urllib.parse import parse_qs
+
+my_values = parse_qs('red=5&blue=0&green=',keep_blank_values=True)
 
