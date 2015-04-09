3bdb6d6442c7d9875fc77934965a1c59bb0520de
hattwick <philhatt@mac.com>
2015-04-08 23:44:46 -0400

    update

--- test3.py
+++ test3.py
@@ -6,3 +6,6 @@ import os
 response = os.popen('ping -c 3 10.0.2.1')
 for line in response.readlines():
     print line
+
+networkdevice  = {"Cisco Nexus 2K":"Access", "Cisco Nexux 5K":"Access Switching","Cisco Nexus 7K":"Core Routing", "Cisco Catalyst 4507":"Access Routing", "Cisco Catalyst 6500":"Aggregate Routing"}
+
