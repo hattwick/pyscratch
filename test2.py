c3478789bc359e3a9bda30e385158887319c73b0
Hattwick <philhattwick@charter.net>
2015-10-04 21:14:22 -0400

    sorting

--- test2.py
+++ test2.py
@@ -41,5 +41,7 @@ def spades_high(card):
     rank_value = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suit_values) + suit_values[card.suit]
 
+for card in sorted(deck):  # doctest: + ELLIPSES
+    print(card)
 
 #End - stopping point
