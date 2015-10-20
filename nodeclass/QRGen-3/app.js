5be313a2f4f05d36717cb4bb2cf8fd20521b586d
Hattwick <philhattwick@charter.net>
2015-10-19 21:03:10 -0400

    new image

--- nodeclass/QRGen-3/app.js
+++ nodeclass/QRGen-3/app.js
@@ -2,7 +2,7 @@ var qrImage = require('qr-image');
 var fs = require('fs');
 
 qrImage
-	.image("http://cagmypctest.wellington.com", {type:'png', size:20})
+	.image("This is what you get with a two pizza team, Jack", {type:'png', size:20})
 	.pipe(fs.createWriteStream("MyQRCode.png"));
 	
 console.log('QR Code Generated !');
