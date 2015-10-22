7a413f8ff691067ebe29214f6afe7793bfe7ae69
Hattwick <philhattwick@charter.net>
2015-10-21 22:15:17 -0400

    update device

--- test3.py
+++ test3.py
@@ -8,4 +8,3 @@ for line in response.readlines():
     print line
 
 networkdevice  = {"Cisco Nexus 2K":"Access", "Cisco Nexux 5K":"Access Switching","Cisco Nexus 7K":"Core Routing", "Cisco Catalyst 4507":"Access Routing", "Cisco Catalyst 6500":"Aggregate Routing"}
-
