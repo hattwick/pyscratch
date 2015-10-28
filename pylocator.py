b07bb2218d2a40d28e187231197826c0c199b14e
Hattwick <philhattwick@charter.net>
2015-10-27 21:58:09 -0400

    starting new locator function
    based on TJ Oconnor Python forensics book

---
+++ pylocator.py
@@ -0,0 +1,10 @@
+# PyGeoIP Locator example
+# Base code from Syngress book by TJ O'Connor with modifications
+
+# Two prerequisites:
+# 1. Local copy of MaxMind GeoLiteCity database
+# 2. pygeoip library by Jennifer Ennis at code.google.com/p/pygeoip
+
+
+
+import pygeoip
