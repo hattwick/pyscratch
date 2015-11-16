425e5f1ba99d416d71678b9b0894aae0b7b1e228
Hattwick <philhattwick@charter.net>
2015-11-15 22:57:53 -0500

    comment error

--- test3.py
+++ test3.py
@@ -8,7 +8,7 @@ response = os.popen('ping -c 3 10.0.2.1')
 
 etworkdevice  = {"Cisco Nexus 2K":"Access", "Cisco Nexux 5K":"Access Switching","Cisco Nexus 7K":"Core Routing", "Cisco Catalyst 4507":"Access Routing", "Cisco Catalyst 6500":"Aggregate Routing"}
 
-print(networkdevice)
+#  print(networkdevice)
 
 for line in response.readlines():
     print(line)
