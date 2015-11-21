bb219f878d0d48d70ef0e5fd3541b9d3b68a9839
Hattwick <philhattwick@charter.net>
2015-11-21 16:55:13 -0500

    update

--- test3.py
+++ test3.py
@@ -6,7 +6,7 @@ import os
 response = os.popen('ping -c 3 10.0.2.1')
 
 
-etworkdevice  = {"Cisco Nexus 2K":"Access", "Cisco Nexux 5K":"Access Switching","Cisco Nexus 7K":"Core Routing", "Cisco Catalyst 4507":"Access Routing", "Cisco Catalyst 6500":"Aggregate Routing"}
+networkdevice  = {"Cisco Nexus 2K":"Access", "Cisco Nexux 5K":"Access Switching","Cisco Nexus 7K":"Core Routing", "Cisco Catalyst 4507":"Access Routing", "Cisco Catalyst 6500":"Aggregate Routing"}
 
 #  print(networkdevice)
 
