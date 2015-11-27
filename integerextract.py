bda4eba5d6727cb863e6ca5b4b01917916dceb57
Hattwick <philhattwick@charter.net>
2015-11-26 23:43:02 -0500

    iterate

--- integerextract.py
+++ integerextract.py
@@ -4,19 +4,23 @@ import re
 import math
 
 datafile = open('data1.txt')
+intlist = list()
 total_integer = 0
 
 print('\n*** Prechecks ***')
 print(total_integer)
-for line in datafile:
-	print(line)
+# for line in datafile:
+#	print(line)
 
 print('\n\n\n *** Beginning extract and sum routine: ***\n')
 
 
 for line in datafile:
-	line = line.rstrip()
-    # intextract = re.findall('([0-9]+)', line)
-    #  print(intextract)
+    intextract = re.findall('[0-9]+', line)
+    if len(intextract) !=1 : continue
+    num = float(intextract[0])
+    print(num)
+    intlist.append(num)
+    
 
 # print('done')
