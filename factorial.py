73661e44ae562c01b4632aed57a421383066395b
Hattwick <philhattwick@charter.net>
2015-11-27 22:27:09 -0500

    test

---
+++ factorial.py
@@ -0,0 +1,10 @@
+import sys
+
+n=10
+
+def Factorial(n): # Return factorial
+    result = 0
+    for i in range (1,n):
+        result = result * i
+    print("factorial is ",result)
+    return result
--- integerextract.py
+++ integerextract.py
@@ -1,4 +1,5 @@
 # Read file, use regex to pull all integers and sum
+# Work examples informed by Coursera, Using Python to Access Web Data
 
 import re
 import math
@@ -9,18 +10,18 @@ total_integer = 0
 
 print('\n*** Prechecks ***')
 print(total_integer)
-# for line in datafile:
-#	print(line)
-
+#
 print('\n\n\n *** Beginning extract and sum routine: ***\n')
 
-
 for line in datafile:
-    intextract = re.findall('[0-9]+', line)
-    if len(intextract) !=1 : continue
-    num = float(intextract[0])
-    print(num)
-    intlist.append(num)
-    
+	#line = line.rstrip()
+	intextract = re.findall('^([0-9])+', line)
+	# if len(intextract) != 1 : continue
+	num = float(intextract[0])
+	print(num)
+	intlist.append(num)
+
+print(intlist)
+	
 
 # print('done')
