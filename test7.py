62276ee1160e4efcbb05297924c950ab083c6865
Hattwick <philhattwick@charter.net>
2015-11-27 23:11:54 -0500

    still debugging

--- test7.py
+++ test7.py
@@ -2,9 +2,28 @@
 
 import re
 
+intlist = list()
+
+
+def listsum(numlist):
+   sum = 0
+   for i in numlist:
+       sum = sum + i
+   return sum
+
+
 hand = open('data1.txt')
 numlist = list()
 for line in hand:
 	line = line.rstrip()
 	stuff = re.findall('([0-9]+)', line)
+	if len(stuff) <1 : continue
 	print(stuff)
+	intlist.append(stuff)
+
+
+#convert to int
+convertedlist = [];
+for n in intlist:
+	convertedlist.append(int(n));
+print(listsum[convertedlist])
