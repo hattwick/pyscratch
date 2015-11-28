f903de0b298d348cca4285a0cdb9bee5dcc1e747
Hattwick <philhattwick@charter.net>
2015-11-28 18:09:09 -0500

    numbers seem better

--- test8.py
+++ test8.py
@@ -7,29 +7,36 @@ intlist = list()
 linetotal = 0
 
 
+# Takes a line of string integers, convert to int and subtotal
 def listsum(numlist):
    sum = 0
    for i in numlist:
-       sum = sum + i
+        try:
+            i=(int(i))
+        except ValueError:
+            pass
+        sum = sum + i
    return sum
 
 
 hand = open('data1.txt')
 numlist = list()
 for line in hand:
-	line = line.rstrip()
-	stuff = re.findall('([0-9]+)', line)
-	if len(stuff) <1 : continue
-	print(stuff)
-	linetotal=linetotal+(listsum(stuff))
-	intlist.append(stuff)
+    line = line.rstrip()
+    stuff = re.findall('([0-9]+)', line)
+    if len(stuff) <1 : continue
+    print(stuff)
+    countitup = (listsum(stuff))
+    linetotal=linetotal+countitup
+    print(linetotal)
+    intlist.append(stuff)
 
 
 #convert to int
-print('Stuff before we change it:\n', stuff)
+#print('Stuff before we change it:\n', stuff)
 convertedlist = [];
 for n in intlist:
-	n = int(n[0])
-	convertedlist.append(n)
+    n = int(n[0])
+    convertedlist.append(n)
 print(convertedlist)
 print(listsum(convertedlist))
