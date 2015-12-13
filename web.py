a8dc63203d0b7774b6f172ace4a418b51c867ca8
Hattwick <philhattwick@charter.net>
2015-12-12 19:55:00 -0500

    adding double loop

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 # Text browser for monitoring, recording and testing
 # Work examples informed by Coursera, Using Python to Access Web Data
-# Note: Week 4 Homework 1 reading web pages
+# Note: Week 4 Homework 2 following a url trail
 
 import urllib
 from BeautifulSoup import BeautifulSoup
@@ -9,7 +9,9 @@ from BeautifulSoup import BeautifulSoup
 # to verify BeautifulSoup version, enter REPL and type:
 # print BeautifulSoup.__version__
 
-url = raw_input('Enter- ')
+url = raw_input('Enter starting URL (http://...): ')
+position = input('Enter the position we are looking for:')
+repeat = input('Enter number of times to iterate:')
 
 html = urllib.urlopen(url).read()
 soup = BeautifulSoup(html)
@@ -19,9 +21,13 @@ soup = BeautifulSoup(html)
 
 tags = soup('a')
 
-
-for tag in tags:
-        print tag.get('href',None)
+for i in range(1,repeat):
+    startposition = 1
+    print 'Starting pass #:', startposition, 'Reapeating ', repeat, ' times:'
+    while startposition < position:
+    	for tag in tags:
+        	print tag.get('href',None)
+        	startposition +=1
 
 
 # RESULT
