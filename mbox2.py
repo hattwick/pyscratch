81f11a2952d9ee5e5bb8346662a7a031b4f789c3
Hattwick <philhattwick@charter.net>
2016-01-09 22:37:55 -0500

    working files

---
+++ mbox2.py
@@ -0,0 +1,22 @@
+#db1   Using Databases with Python - homework for week 2
+
+import sqlite3
+import re
+
+
+fname = raw_input('Enter file name: ')
+if ( len(fname) < 1 ) : fname = 'mbox-short.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: ') : continue
+    pieces = line.split()
+    email = pieces[3]
+    orgtemp = re.search("@([\w.]+)",email).group(0)
+    orgstring = str(orgtemp)
+    org = re.sub('@','', orgstring)
+#   org = pieces[1]
+    print org
+ 
+
+# org = re.search("@([\w.]+)",email).group(0)
+#     org = re.findall('@([\w.]+)', email)
