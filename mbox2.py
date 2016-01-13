d81b2d2ccb303ee26fac452ed0f4aeb0e9934374
Hattwick <philhattwick@charter.net>
2016-01-12 23:18:13 -0500

    misc

--- mbox2.py
+++ mbox2.py
@@ -4,17 +4,20 @@ import sqlite3
 import re
 
 
-fname = raw_input('Enter file name: ')
+
+# fname = raw_input('Enter file name: ')
+fname = 'mbox-short.txt'
 if ( len(fname) < 1 ) : fname = 'mbox-short.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: ') : continue
     pieces = line.split()
-    email = pieces[0]
+    email = pieces[1]
     orgtemp = re.search("@([\w.]+)",email).group(0)
     orgstring = str(orgtemp)
     org = re.sub('@','', orgstring)
 #   org = pieces[1]
+    print email
     print org
  
 
---
+++ mbox4.py
@@ -0,0 +1,26 @@
+import re
+
+# name = raw_input("Enter file:")
+name = "mbox-short.txt"
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+counts = dict()
+
+hour_regex= re.compile(r'(\d\d):')
+
+for line in handle:
+    if not line.startswith('From') : continue
+    time = line.split('Jan')
+    hourfind = hour_regex.search(time)
+    hrs = hourfind.group(1) 
+    print 'time:', time
+    print 'hour:', hrs
+    for word in time:
+        counts[word] =counts.get(word,0) + 1
+
+
+print '\nPrinting last time after loop\n'
+print time
+print '\n now printing counts....'
+print counts.items()
