82d1c815443052d6f930f6d97df2a294644c15d9
hattwick <philhatt@mac.com>
2016-03-19 16:25:20 -0400

    full xml parse

--- xmltest.py
+++ xmltest.py
@@ -1,9 +1,27 @@
 from xml.etree import ElementTree as ET
 
-tree = ET.parse('data.xml')
+tree = ET.parse('data-text.xml')
 root = tree.getroot()
-print dir(root)
+# print dir(root)
 print '****'
-print list(root)
+# print list(root)
 
-data = root.find('')
+data = root.find('Data')
+print list(data)
+
+for observation in data:
+    for item in observation:
+        print item
+        print item.text
+        print list(item)
+        print item.attrib
+
+print '*********\n'
+
+all_data=[]
+
+for observation in data:
+    record = {}
+    for item in observation:
+        print item.attrib
+
