fc2c56e37919c28763e01809c7eec16174bacfb7
Hattwick <philhattwick@charter.net>
2016-06-24 22:25:17 -0400

    python finals

---
+++ capstone/gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Month','umich.edu','indiana.edu','berkeley.edu','ucdavis.edu','uct.ac.za','mac.com','columbia.edu','ufp.pt','gmail.com','cam.ac.uk'],
+['2005-12',57,12,12,11,14,12,13,10,10,7],
+['2006-01',114,32,33,37,27,14,28,32,23,19],
+['2006-02',41,12,9,4,8,20,4,3,10,12]
+];
