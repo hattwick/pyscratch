343be7390a7ceadc60acdcda670449562e82aaf5
hattwick <phil@hattwick.com>
2017-04-27 21:57:41 -0400

    initial graph generate

--- netpath.py
+++ netpath.py
@@ -4,7 +4,7 @@
 # First the function approach
 
 graph = { "HQ" : ["DC1", "DC2"],
-          "BackOffice: ["DC1", "DC2"],
+          "BackOffice" : ["DC1", "DC2"],
           "DC1" : ["HQ","BackOffice", "DC2","Colo1", "Colo2", "Colo3"],
           "DC2": ["HQ", "BackOffice", "DC1", "Colo1", "Colo2", "Colo3"],
           "Colo1": ["DC2", "Colo3"],
@@ -12,3 +12,14 @@ graph = { "HQ" : ["DC1", "DC2"],
           "Colo3": ["Colo1"]
           }
 
+print('Starting graph analysis')
+print('This is printing the old way')
+
+def generate_edge(graph):
+    edges = []
+    for node in graph:
+        for neighbor in graph[node]:
+            edges.append((node, neighbor))
+    return edges
+
+print(generate_edge(graph))
