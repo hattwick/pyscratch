f7b963fbf736644ac4669d02ceb8a02768a784a0
hattwick <phil@hattwick.com>
2017-04-27 22:55:50 -0400

    update - need to determine why key has 5 elements

--- netpath.py
+++ netpath.py
@@ -1,15 +1,16 @@
 # Simple graph theory to check for network isolation and survivability scenarios
 # Great tip on switching from functions to a graph class from Berd Klein at Bodenseo
 
-# First the function approach
+# This is the function approach
 
 graph = { "HQ" : ["DC1", "DC2"],
           "BackOffice" : ["DC1", "DC2"],
           "DC1" : ["HQ","BackOffice", "DC2","Colo1", "Colo2", "Colo3"],
-          "DC2": ["HQ", "BackOffice", "DC1", "Colo1", "Colo2", "Colo3"],
-          "Colo1": ["DC2", "Colo3"],
-          "Colo2": ["DC1"],
-          "Colo3": ["Colo1"]
+          "DC2" : ["HQ", "BackOffice", "DC1", "Colo1", "Colo2", "Colo3"],
+          "Colo1" : ["DC2", "Colo3"],
+          "Colo2" : ["DC1"],
+          "Colo3" : ["Colo1"],
+          "Colo4" : []
           }
 
 print('Starting graph analysis')
@@ -22,4 +23,17 @@ def generate_edge(graph):
             edges.append((node, neighbor))
     return edges
 
-print(generate_edge(graph))
+def isolated_node_check(graph):
+    isolated = []
+    for node in graph:
+        if not graph[node]:
+            isolated += node
+    return isolated
+
+
+
+print('\nGenerating node graph: \n', generate_edge(graph), '\n')
+
+print('\nChecking for isolated nodes: \n', isolated_node_check(graph), '\n')
+
+
